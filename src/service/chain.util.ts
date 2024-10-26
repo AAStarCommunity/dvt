6abf2e14132ba2f81fde0f3fcf5c590c4496e6ac
0xcy <993921@qq.com>
2024-10-26 15:42:07 +0800

    code clean

--- src/service/chain.util.ts
+++ src/service/chain.util.ts
@@ -4,88 +4,6 @@ import * as fs from "fs";
 
 import abi from './signature.verification.abi.json';
 
-const abi2 = [
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "_message",
-                "type": "string"
-            }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-    },
-    {
-        "inputs": [],
-        "name": "getMessage",
-        "outputs": [
-            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "message",
-        "outputs": [
-            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "_message",
-                "type": "string"
-            }
-        ],
-        "name": "setMessage",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "uint256[2]",
-                "name": "signature",
-                "type": "uint256[2]"
-            },
-            {
-                "internalType": "uint256[4][]",
-                "name": "pubkeys",
-                "type": "uint256[4][]"
-            },
-            {
-                "internalType": "uint256[2][]",
-                "name": "messages",
-                "type": "uint256[2][]"
-            }
-        ],
-        "name": "validateAggregatorSignature",
-        "outputs": [
-            {
-                "internalType": "bool",
-                "name": "",
-                "type": "bool"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    }
-];
-
 if (fs.existsSync('.env.local')) {
     dotenv.config({ path: '.env.local' });
   } else {
@@ -100,7 +18,7 @@ const wallet = new ethers.Wallet(privateKey, provider);
 
 const contractAddress = process.env.ADD || "";
 
-const contract = new ethers.Contract(contractAddress, abi2, wallet);
+const contract = new ethers.Contract(contractAddress, abi, wallet);
 
 export async function verifyAggrSigs(signature: string[2], pubkeys: string[4][], messages: any[]): Promise<boolean> {
     const result = await contract.validateAggregatorSignature(signature, pubkeys, messages);
